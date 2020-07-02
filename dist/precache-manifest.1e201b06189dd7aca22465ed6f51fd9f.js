145721eede8080cf7bbbb394397eed6653f80e82
Yash Trivedi <t.yash2003@gmail.com>
2020-07-02 02:39:35 -0400

    Build

---
+++ dist/precache-manifest.1e201b06189dd7aca22465ed6f51fd9f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77d55febd25f56e5662",
+    "url": "/css/app.9ead9a11.css"
+  },
+  {
+    "revision": "df6aa2a21ca7fbc72da1cb0564ecc981",
+    "url": "/img/download.png"
+  },
+  {
+    "revision": "2d7de5061642b8973b8fe1675040061c",
+    "url": "/img/lectio.png"
+  },
+  {
+    "revision": "94b0dcefd9e68dbf5af1803516d46622",
+    "url": "/img/lectio.svg"
+  },
+  {
+    "revision": "c8edbc956a93fc13ff9938f5f467b746",
+    "url": "/img/prepri.svg"
+  },
+  {
+    "revision": "15e19c342cf1397703d23a76841f1cd5",
+    "url": "/img/site-demo.gif"
+  },
+  {
+    "revision": "0bb603f3653d1e496bfb77a86ed730ab",
+    "url": "/img/site.svg"
+  },
+  {
+    "revision": "bd0dbd918eb77bdda41dcca08338fe64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77d55febd25f56e5662",
+    "url": "/js/app.e036694b.js"
+  },
+  {
+    "revision": "ada91a5492397d118c8e",
+    "url": "/js/chunk-vendors.ca88229e.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c4fff550bfbd5672a034801d601c9a10",
+    "url": "/projects.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
